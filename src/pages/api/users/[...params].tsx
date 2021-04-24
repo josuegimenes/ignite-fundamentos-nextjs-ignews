3e239e49494532b48dc71668614f367c0cf9723a
Josué Gimenes <josuegalvares@gmail.com>
2021-04-23 21:43:36 -0300

    Parameterization on routes

---
+++ src/pages/api/users/[...params].tsx
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+    console.log(request.query)
+
+    const users = [
+        { id: 1, name: 'Josué' },
+        { id: 2, name: 'Diego' },
+        { id: 3, name: 'Dani' },
+        { id: 4, name: 'Rafa' },
+    ]
+
+    return response.json(users);
+}
