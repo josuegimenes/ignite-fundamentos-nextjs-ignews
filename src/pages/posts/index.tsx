1a0c109121f2138d2a448b7ca4e3842d562cdf4f
Josué Gimenes <josuegalvares@gmail.com>
2021-05-22 23:16:00 -0300

    Validating active subscription

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -47,9 +47,9 @@ export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient()
 
     const response = await prismic.query([
-        Prismic.predicates.at('document.type', 'publication')
+        Prismic.predicates.at('document.type', 'post')
     ], {
-        fetch: ['publication.title', 'publication.content'],
+        fetch: ['post.title', 'post.content'],
         pageSize: 100,
     })
 
@@ -68,6 +68,8 @@ export const getStaticProps: GetStaticProps = async () => {
     
 
     return {
-        props: {}
+        props: {
+            posts
+        }
     }
 }
