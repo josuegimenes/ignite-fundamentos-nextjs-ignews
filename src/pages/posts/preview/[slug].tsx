e7038bd11582eaf86cce778c7bd183dafeb8d617
Josué Gimenes <josuegalvares@gmail.com>
2021-05-29 16:10:14 -0300

    Updating - Generating static previews

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -58,9 +58,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
 
 export const getStaticPaths: GetStaticPaths = async () => {
     return {
-        paths: [
-            { params: { slug: 'mapas-com-react-usando-leaflet' } }
-        ],
+        paths: [],
         fallback: 'blocking'
     }
 }
